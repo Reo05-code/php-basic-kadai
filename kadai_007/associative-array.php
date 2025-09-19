f7370e7a16d573b89d915d9a6c2367ed792ffa02
Reo05-code <t.asaishi05@gmail.com>
2025-09-19 12:05:12 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+   <meta charset="UTF-8">
+   <title>Document</title>
+</head>
+<body>
+   <p>
+      <?php
+      $vegetable = [
+         "name" => "onion",
+         "price" => 20,
+         "weight" => 160
+      ];
+
+      echo '<br>';
+
+      print_r($vegetable);
+
+      ?>
+   </p>
+</body>
+</html>
